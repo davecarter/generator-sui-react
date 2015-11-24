b871f723fd529c66846adbdc119713cd52f62c31
Carlos Villuendas <carlosvillu@gmail.com>
2015-11-24 16:29:22 +0100

    add env vars

--- app/templates/_webpack.config.js
+++ app/templates/_webpack.config.js
@@ -28,6 +28,11 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+          'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+       }
+    })
   ]
 };
--- app/templates/_webpack.doc.config.js
+++ app/templates/_webpack.doc.config.js
@@ -23,5 +23,12 @@ module.exports = {
         loader: 'style!css!sass'
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+          'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+       }
+    })
+  ]
 };
