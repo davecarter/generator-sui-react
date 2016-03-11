ada6be7c3222c569e3b3f5f1301034a70b3efd38
Daniel de la Cruz <daniel.delacruz@scmspain.com>
2016-03-11 07:32:19 +0100

    improved webpack and package json tasks for bundling

--- app/templates/_webpack.config.js
+++ app/templates/_webpack.config.js
@@ -1,38 +1,86 @@
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var merge = require('webpack-merge');
 
-module.exports = {
-  entry: [
-    'webpack-dev-server/client?http://0.0.0.0:8080',
-    'webpack/hot/only-dev-server',
-    path.resolve(__dirname, 'docs/index.jsx')
-  ],
-  output: {
-    path: path.resolve(__dirname, 'docs/dist'),
-    filename: 'dist/index.js'
-  },
+var APP_PATH = path.join(__dirname, '/src');
+var TARGET = process.env.npm_lifecycle_event;
+
+var path = require('path');
+var webpack = require('webpack');
+
+var base = {
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
   module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        loaders: ['react-hot-loader', 'babel-loader'],
-        exclude: path.join(__dirname, 'node_modules')
-      },
-      {
-        test: /\.scss$/,
-        loader: 'style!css!sass'
-      }
-    ]
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['babel-loader'],
+      exclude: path.join(__dirname, 'node_modules')
+    }]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-          'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
        }
     })
   ]
-};
+}
+
+if(TARGET === 'start:server' || !TARGET) {
+  module.exports = merge(base, {
+    entry: [
+      'webpack-dev-server/client?http://0.0.0.0:8080',
+      'webpack/hot/only-dev-server',
+      path.resolve(__dirname, 'docs/index.jsx')
+    ],
+    output: {
+      path: path.resolve(__dirname, 'docs/dist'),
+      filename: 'dist/index.js'
+    },
+    devServer: {
+      port: 8080,
+      stats: { colors: true },
+      inline: true,
+      contentBase: 'docs'
+    },
+    devtool: 'source-map',
+    module: {
+      loaders: [{
+        test: /\.s?css$/,
+        loader: 'style!css!sass'
+      }]
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
+
+
+if(TARGET === 'bundle') {
+  module.exports = merge(base, {
+    entry: path.resolve(__dirname, 'docs/index.jsx'),
+    output: {
+      filename: 'bundle/index.js'
+    },
+    module: {
+      loaders: [{
+        test: /\.s?css$/,
+        loader: ExtractTextPlugin.extract('css!sass')
+      }]
+    },
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      }),
+      new ExtractTextPlugin('bundle/index.css', {
+        allChunks: true
+      })
+    ]
+  });
+}
